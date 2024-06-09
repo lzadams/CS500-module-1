b993d6d58c20b698185b2ee62dd9bc3df6e0106f
lzadams <lili.adams@csuglobal.edu>
2024-06-08 18:21:22 -0700

    Update Module1Part1.py

--- Module1_part1.py
+++ Module1_part1.py
@@ -1,12 +1,12 @@
 #CS500 Module1 assigment Part1
 #input 2 numbers
-num1 = input()
-num2 = input()
+print('Enter num1:')
+num1 = float(input())
+print('Enter num2:')
+num2 = float(input())
 #processing
 sum = num1 + num2
 subtract = num1 - num2
 #output
-print(sum)
-print(subtract)
-
-
+print('sum =',sum)
+print('subtract =',subtract)
