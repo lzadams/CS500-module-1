bd33520e6b34fc134771a2703944977a08442312
lzadams <lili.adams@csuglobal.edu>
2024-06-08 18:16:22 -0700

    Add files via upload

---
+++ Module1Part2.py
@@ -0,0 +1,12 @@
+#CS500 Module1 assigment Part2
+#input 2 numbers
+print('Enter num1=')
+num1 = float(input())
+print('Enter num2=')
+num2 = float(input())
+#processing
+mutiply = num1 * num2
+divide = num1/num2
+#output
+print('multiply =',mutiply)
+print('divide =',divide)
